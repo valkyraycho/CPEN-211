55a5f27261692ba81798da49eafedbca5f1a1756
Ray Cho <razor0424@gmail.com>
2024-08-07 22:08:13 +0800

    lab6 refactor

--- lab6/controller.sv
+++ lab6/controller.sv
@@ -1,73 +1,202 @@
-module controller(input clk, input rst_n, input start,
-                  input [2:0] opcode, input [1:0] ALU_op, input [1:0] shift_op,
-                  input Z, input N, input V,
-                  output waiting,
-                  output [1:0] reg_sel, output [1:0] wb_sel, output w_en,
-                  output en_A, output en_B, output en_C, output en_status,
-                  output sel_A, output sel_B);
-    reg [1:0] sel_reg, sel_wb;
-    assign reg_sel = sel_reg, wb_sel = sel_wb;
-    reg Wait, en_w, A_en, B_en, C_en, status_en, A_sel, B_sel;
-    assign waiting = Wait, w_en = en_w, en_A = A_en, en_B = B_en, en_C = C_en, en_status = status_en, sel_A = A_sel, sel_B = B_sel;
-    enum reg [2:0] {WAIT, READ_INSTR, WRITE_VALUE, READ_VALUEA, READ_VALUEB , ENABLE_C} state;
+module controller (
+    input              clk,
+    input              rst_n,
+    input              start,
+    input        [2:0] opcode,
+    input        [1:0] ALU_op,
+    input        [1:0] shift_op,
+    input              Z,
+    input              N,
+    input              V,
+    output logic       waiting,
+    output logic [1:0] reg_sel,
+    output logic [1:0] wb_sel,
+    output logic       w_en,
+    output logic       en_A,
+    output logic       en_B,
+    output logic       en_C,
+    output logic       en_status,
+    output logic       sel_A,
+    output logic       sel_B
+);
+    typedef enum {
+        WAIT,
+        READ_INSTR,
+        WRITE_VALUE,
+        READ_VALUEA,
+        READ_VALUEB,
+        ENABLE_C
+    } state_t;
 
-    always_comb
-    begin
-        casex({state, opcode, ALU_op})
-            {WAIT, 3'bxxx, 2'bxx}: {Wait, sel_reg, sel_wb, en_w, A_en, B_en, C_en, status_en, A_sel, B_sel} = {1'b1, 11'b0};
-            //move_immediate
-            {READ_INSTR, 3'b110, 2'b10}: {sel_wb, Wait, sel_reg, en_w, A_en, B_en, C_en, status_en, A_sel, B_sel} = {2'b10, 10'b0};
-            {WRITE_VALUE, 3'b110, 2'b10}: {sel_reg, en_w, sel_wb, Wait, A_en, B_en, C_en, status_en, A_sel, B_sel} = {2'b10, 1'b1, 2'b10, 7'b0};
-            //MOV
-            {READ_INSTR, 3'b110, 2'b00}: {sel_wb, Wait, sel_reg, en_w, A_en, B_en, C_en, status_en, A_sel, B_sel} = {2'b00, 10'b0};
-            {READ_VALUEB, 3'b110, 2'b00}: {sel_reg, B_en, sel_wb, Wait, en_w, A_en, C_en, status_en, A_sel, B_sel} = {2'b00, 1'b1, 9'b0}; //{sel_reg, B_en} = {2'b00, 1'b1};
-            {ENABLE_C, 3'b110, 2'b00}: {C_en, status_en, sel_wb, Wait, sel_reg, en_w, A_en, B_en, A_sel, B_sel} = {2'b11, 8'b0, 2'b10}; //{C_en, status_en} = 2'b11;
-            {WRITE_VALUE, 3'b110, 2'b00}: {sel_reg, en_w, sel_wb, Wait, A_en, B_en, C_en, status_en, A_sel, B_sel} = {2'b01, 1'b1, 9'b0}; //{sel_reg, en_w} = {2'b01, 1'b1};
-            //ADD
-            {READ_INSTR, 3'b101, 2'b00}: {sel_wb, Wait, sel_reg, en_w, A_en, B_en, C_en, status_en, A_sel, B_sel} = {2'b00, 10'b0};
-            {READ_VALUEA, 3'b101, 2'b00}: {sel_reg, A_en, sel_wb, Wait, en_w, B_en, C_en, status_en, A_sel, B_sel} = {2'b10, 1'b1, 9'b0}; //{sel_reg, A_en} = {2'b01, 1'b1};
-            {READ_VALUEB, 3'b101, 2'b00}: {sel_reg, A_en, B_en, sel_wb, Wait, en_w, C_en, status_en, A_sel, B_sel} = {2'b00, 1'b0, 1'b1, 8'b0}; //{sel_reg, A_en, B_en} = {2'b00, 1'b0, 1'b1};
-            {ENABLE_C, 3'b101, 2'b00}: {C_en, status_en, sel_wb, Wait, sel_reg, en_w, A_en, B_en, A_sel, B_sel} = {2'b11, 10'b0};
-            {WRITE_VALUE, 3'b101, 2'b00}: {sel_reg, en_w, sel_wb, Wait, A_en, B_en, C_en, status_en, A_sel, B_sel} = {2'b01, 1'b1, 9'b0}; //{sel_reg, en_w} = {2'b01, 1'b1};
-            //CMP
-            {READ_INSTR, 3'b101, 2'b01}: {sel_wb, Wait, sel_reg, en_w, A_en, B_en, C_en, status_en, A_sel, B_sel} = {2'b00, 10'b0};
-            {READ_VALUEA, 3'b101, 2'b01}: {sel_reg, A_en, sel_wb, Wait, en_w, B_en, C_en, status_en, A_sel, B_sel} = {2'b10, 1'b1, 9'b0}; //{sel_reg, A_en} = {2'b10, 1'b1};
-            {READ_VALUEB, 3'b101, 2'b01}: {sel_reg, A_en, B_en, sel_wb, Wait, en_w, C_en, status_en, A_sel, B_sel} = {2'b00, 1'b0, 1'b1, 8'b0}; //{sel_reg, A_en, B_en} = {2'b00, 1'b0, 1'b1};
-            {ENABLE_C, 3'b101, 2'b01}: {C_en, status_en, sel_wb, Wait, sel_reg, en_w, A_en, B_en, A_sel, B_sel} = {2'b01, 10'b0};
-            //AND
-            {READ_INSTR, 3'b101, 2'b10}: {sel_wb, Wait, sel_reg, en_w, A_en, B_en, C_en, status_en, A_sel, B_sel} = {2'b00, 10'b0};
-            {READ_VALUEA, 3'b101, 2'b10}: {sel_reg, A_en, sel_wb, Wait, en_w, B_en, C_en, status_en, A_sel, B_sel} = {2'b10, 1'b1, 9'b0}; //{sel_reg, A_en} = {2'b01, 1'b1};
-            {READ_VALUEB, 3'b101, 2'b10}: {sel_reg, A_en, B_en, sel_wb, Wait, en_w, C_en, status_en, A_sel, B_sel} = {2'b00, 1'b0, 1'b1, 8'b0}; //{sel_reg, A_en, B_en} = {2'b00, 1'b0, 1'b1};
-            {ENABLE_C, 3'b101, 2'b10}: {C_en, status_en, sel_wb, Wait, sel_reg, en_w, A_en, B_en, A_sel, B_sel} = {2'b11, 10'b0};
-            {WRITE_VALUE, 3'b101, 2'b10}: {sel_reg, en_w, sel_wb, Wait, A_en, B_en, C_en, status_en, A_sel, B_sel} = {2'b01, 1'b1, 9'b0}; //{sel_reg, en_w} = {2'b10, 1'b1};
-            //MVN
-            {READ_INSTR, 3'b101, 2'b11}: {sel_wb, Wait, sel_reg, en_w, A_en, B_en, C_en, status_en, A_sel, B_sel} = {2'b00, 10'b0};
-            {READ_VALUEB, 3'b101, 2'b11}: {sel_reg, B_en, sel_wb, Wait, en_w, A_en, C_en, status_en, A_sel, B_sel} = {2'b00, 1'b1, 9'b0}; //{sel_reg, B_en} = {2'b00, 1'b1};
-            {ENABLE_C, 3'b101, 2'b11}: {C_en, status_en, sel_wb, Wait, sel_reg, en_w, A_en, B_en, A_sel, B_sel} = {2'b11, 8'b0, 2'b10}; //{C_en, status_en} = 2'b11;
-            {WRITE_VALUE, 3'b101, 2'b11}: {sel_reg, en_w, sel_wb, Wait, A_en, B_en, C_en, status_en, A_sel, B_sel} = {2'b01, 1'b1, 9'b0}; //{sel_reg, en_w} = {2'b01, 1'b1};
-            default {Wait, sel_reg, sel_wb, en_w, A_en, B_en, C_en, status_en, A_sel, B_sel} = 12'b000000000000;
+    state_t state, next_state;
+
+    always_ff @(posedge clk) begin
+        if (!rst_n) begin
+            state <= WAIT;
+        end
+        else begin
+            state <= next_state;
+        end
+    end
+
+
+    always_comb begin
+        waiting    = 1'b0;
+        reg_sel    = 2'b0;
+        wb_sel     = 2'b0;
+        w_en       = 1'b0;
+        en_A       = 1'b0;
+        en_B       = 1'b0;
+        en_C       = 1'b0;
+        en_status  = 1'b0;
+        sel_A      = 1'b0;
+        sel_B      = 1'b0;
+        next_state = state;
+        case (state)
+            WAIT: begin
+                waiting = 1'b1;
+                if (start) next_state = READ_INSTR;
+            end
+
+            READ_INSTR: begin
+                case ({
+                    opcode, ALU_op
+                })
+                    {
+                        3'b110, 2'b10  // MOV_imm
+                    } : begin
+                        wb_sel     = 2'b10;
+                        next_state = WRITE_VALUE;
+                    end
+                    {
+                        3'b110, 2'b00  // MOV
+                    } : begin
+                        wb_sel     = 2'b00;
+                        next_state = READ_VALUEB;
+                    end
+                    {
+                        3'b101, 2'b00  // ADD
+                    } : begin
+                        wb_sel     = 2'b00;
+                        next_state = READ_VALUEA;
+                    end
+                    {
+                        3'b101, 2'b01  // CMP
+                    } : begin
+                        wb_sel     = 2'b00;
+                        next_state = READ_VALUEA;
+                    end
+                    {
+                        3'b101, 2'b10  // AND
+                    } : begin
+                        wb_sel     = 2'b00;
+                        next_state = READ_VALUEA;
+                    end
+                    {
+                        3'b101, 2'b11  // MVN
+                    } : begin
+                        wb_sel     = 2'b00;
+                        next_state = READ_VALUEB;
+                    end
+                endcase
+            end
+
+            WRITE_VALUE: begin
+                next_state = WAIT;
+                w_en       = 1'b1;
+                case ({
+                    opcode, ALU_op
+                })
+                    {
+                        3'b110, 2'b10  // MOV_imm
+                    } : begin
+                        reg_sel = 2'b10;
+                        wb_sel  = 2'b10;
+                    end
+                    {
+                        3'b110, 2'b00  // MOV
+                    } : begin
+                        reg_sel = 2'b01;
+                        wb_sel  = 2'b00;
+                    end
+                    {
+                        3'b101, 2'b00  // ADD
+                    } : begin
+                        reg_sel = 2'b01;
+                        wb_sel  = 2'b00;
+                    end
+                    {
+                        3'b101, 2'b10  // AND
+                    } : begin
+                        reg_sel = 2'b01;
+                        wb_sel  = 2'b00;
+                    end
+                    {
+                        3'b101, 2'b11  // MVN
+                    } : begin
+                        reg_sel = 2'b01;
+                        wb_sel  = 2'b00;
+                    end
+                endcase
+            end
+
+            READ_VALUEA: begin
+                next_state = READ_VALUEB;
+                en_A       = 1'b1;
+                case ({
+                    opcode, ALU_op
+                })
+                    {3'b101, 2'b00} : reg_sel = 2'b10;  // ADD
+                    {3'b101, 2'b01} : reg_sel = 2'b10;  // CMP
+                    {3'b101, 2'b10} : reg_sel = 2'b10;  // AND
+                endcase
+            end
+
+            READ_VALUEB: begin
+                next_state = ENABLE_C;
+                en_B       = 1'b1;
+                reg_sel    = 2'b00;
+            end
+
+            ENABLE_C: begin
+                next_state = WRITE_VALUE;
+                en_C       = 1'b1;
+                case ({
+                    opcode, ALU_op
+                })
+                    {
+                        3'b110, 2'b00  // MOV
+                    } : begin
+                        wb_sel = 2'b10;
+                        sel_A  = 1'b1;
+                    end
+                    {
+                        3'b101, 2'b00  // ADD
+                    } : begin
+                        wb_sel = 2'b00;
+                    end
+                    {
+                        3'b101, 2'b01  // CMP
+                    } : begin
+                        en_C       = 1'b0;
+                        en_status  = 1'b1;
+                        wb_sel     = 2'b00;
+                        next_state = WAIT;
+                    end
+                    {
+                        3'b101, 2'b10  // AND
+                    } : begin
+                        wb_sel = 2'b00;
+                    end
+                    {
+                        3'b101, 2'b11  // MVN
+                    } : begin
+                        wb_sel = 2'b00;
+                        sel_A  = 1'b1;
+                    end
+                endcase
+            end
         endcase
     end
 
-    always_ff @(posedge clk)
-    begin
-        if(~rst_n)
-        begin
-            state <= WAIT;
-        end
-        else 
-        begin
-            case(state)
-                WAIT: if(start) state <= READ_INSTR;
-                READ_INSTR: if(opcode == 3'b110 && ALU_op == 2'b10) state <= WRITE_VALUE;
-                              else if((opcode == 3'b110 && ALU_op == 2'b00) || (opcode == 3'b101 && ALU_op == 2'b11)) state <= READ_VALUEB;
-                              else state <= READ_VALUEA;
-                WRITE_VALUE: state <= WAIT;
-                READ_VALUEA: state <= READ_VALUEB;
-                READ_VALUEB: state <= ENABLE_C;
-                ENABLE_C: if(opcode == 3'b101 && ALU_op == 2'b01) state <= WAIT;
-                          else state <= WRITE_VALUE;
-            endcase
-        end
-    end
-endmodule: controller
+endmodule : controller
--- lab6/tb_cpu.sv
+++ lab6/tb_cpu.sv
@@ -1,4 +1,6 @@
-module tb_cpu(output err);
+module tb_cpu (
+    output err
+);
     reg clk, rst_n, load, start;
     reg [15:0] instr;
     wire waiting, N, V, Z;
@@ -9,34 +11,69 @@ module tb_cpu(output err);
     reg error;
     assign err = error;
 
-    task reset; rst_n = 1'b0; #5; clk = 1'b1; #5; rst_n = 1'b1; clk = 1'b0; #5; endtask
-    task clock; clk = 1'b1; #5; clk = 1'b0; #5; endtask
-    task initiate; load = 1'b1; start = 1'b1; #5; clock; start = 1'b0; load = 1'b0;  #5; endtask
-    task set_value([15:0] ir); instr = ir; #5; initiate; clock; clock;  endtask
-    cpu dut(.clk, .rst_n, .load, .start, .instr, .waiting, .out, .N, .V, .Z);
-    initial
-    begin
-        {clk, load, start} = 3'b0;
+    task reset;
+        rst_n = 1'b0;
+        #5;
+        clk = 1'b1;
+        #5;
         rst_n = 1'b1;
-        instr = 16'b0;
-        numfail = 1'b0;
-        numpass = 1'b0;
-        error = 1'b0;
+        clk   = 1'b0;
+        #5;
+    endtask
+    task clock;
+        clk = 1'b1;
+        #5;
+        clk = 1'b0;
+        #5;
+    endtask
+    task initiate;
+        load  = 1'b1;
+        start = 1'b1;
+        #5;
+        clock;
+        start = 1'b0;
+        load  = 1'b0;
+        #5;
+    endtask
+    task set_value([15:0] ir);
+        instr = ir;
+        #5;
+        initiate;
+        clock;
+        clock;
+    endtask
+    cpu dut (
+        .clk,
+        .rst_n,
+        .load,
+        .start,
+        .instr,
+        .waiting,
+        .out,
+        .N,
+        .V,
+        .Z
+    );
+    initial begin
+        {clk, load, start} = 3'b0;
+        rst_n              = 1'b1;
+        instr              = 16'b0;
+        numfail            = 1'b0;
+        numpass            = 1'b0;
+        error              = 1'b0;
         #5;
 
         reset;
-        assert(waiting === 1'b1)
-        begin
+        assert (waiting === 1'b1) begin
             $display("[PASS] reset to wait");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] reset to wait");
             numfail = numfail + 1;
-            error = 1'b1;
+            error   = 1'b1;
         end
-        
+
         set_value(16'b1101000000000001);
         set_value(16'b1101000100000010);
         set_value(16'b1101001000000011);
@@ -46,147 +83,155 @@ module tb_cpu(output err);
         set_value(16'b1101011000000111);
         set_value(16'b1101011100001000);
 
-        
+
 
         //MOV r2 r6
         instr = 16'b1100000001000110;
         initiate;
-        clock; clock; clock; 
-        assert(waiting === 1'b0)
-        begin
+        clock;
+        clock;
+        clock;
+        assert (waiting === 1'b0) begin
             $display("[PASS] waiting is 0 during calculation");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] waiting is 0 during calculation");
             numfail = numfail + 1;
-            error = 1'b1;
+            error   = 1'b1;
         end
-        
+
         clock;
-        assert(out === 16'd7 && {Z, V, N} === 3'b000 && waiting === 1'b1)
-        begin
+        assert (out === 16'd7 && waiting === 1'b1) begin
             $display("[PASS] MOV r2 r6");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] MOV r2 r6");
             numfail = numfail + 1;
-            error = 1'b1;
+            error   = 1'b1;
         end
-        
+
         //ADD r7 r2 r6
         instr = 16'b1010001011100110;
         initiate;
-        clock; clock; clock; clock; clock; 
-        assert(out === 16'd14 && {Z, V, N} === 3'b000 && waiting === 1'b1)
-        begin
+        clock;
+        clock;
+        clock;
+        clock;
+        clock;
+        assert (out === 16'd14 && waiting === 1'b1) begin
             $display("[PASS] ADD r7 r2 r6");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] ADD r7 r2 r6");
             numfail = numfail + 1;
-            error = 1'b1;
+            error   = 1'b1;
         end
-        
+
         //CMP r3 r5
         instr = 16'b1010101100000101;
         initiate;
-        clock; clock; clock; clock; 
-        assert({Z, V, N} === 3'b001 && waiting === 1'b1)
-        begin
+        clock;
+        clock;
+        clock;
+        clock;
+        assert ({Z, V, N} === 3'b001 && waiting === 1'b1) begin
             $display("[PASS] CMP r3 r5");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] CMP r3 r5");
             numfail = numfail + 1;
-            error = 1'b1;
-        end 
+            error   = 1'b1;
+        end
 
         //AND r3 r5 r0
         instr = 16'b1011001110100000;
         initiate;
-        clock; clock; clock; clock; clock; 
-        assert(out === 16'b0 && {Z, V, N} === 3'b000 && waiting === 1'b1)
-        begin
+        clock;
+        clock;
+        clock;
+        clock;
+        clock;
+        assert (out === 16'b0 && waiting === 1'b1) begin
             $display("[PASS] AND r3 r5 r0");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] AND r3 r5 r0");
             numfail = numfail + 1;
-            error = 1'b1;
-        end 
+            error   = 1'b1;
+        end
 
         //MVN r7 r4
         instr = 16'b1011100011100100;
         initiate;
-        clock; clock; clock; clock;      
-        assert(out === 16'b1111111111111010 && {Z, V, N} === 3'b000 && waiting === 1'b1)
-        begin
+        clock;
+        clock;
+        clock;
+        clock;
+        assert (out === 16'b1111111111111010 && waiting === 1'b1) begin
             $display("[PASS] MVN r7 r4");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] MVN r7 r4");
             numfail = numfail + 1;
-            error = 1'b1;
+            error   = 1'b1;
         end
 
         //MOV r2 r1 lsl#1
         instr = 16'b1100000001001001;
         initiate;
-        clock; clock; clock; clock; 
-        assert(out === 16'd4 && {Z, V, N} === 3'b000 && waiting === 1'b1)
-        begin
+        clock;
+        clock;
+        clock;
+        clock;
+        assert (out === 16'd4 && waiting === 1'b1) begin
             $display("[PASS] MOV r2 r1 lsl#1");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] MOV r2 r1 lsl#1");
             numfail = numfail + 1;
-            error = 1'b1;
+            error   = 1'b1;
         end
 
         //ADD r7 r2 r6 lsr#1
         instr = 16'b1010001011110110;
         initiate;
-        clock; clock; clock; clock; clock; 
-        assert(out === 16'd7 && {Z, V, N} === 3'b000 && waiting === 1'b1)
-        begin
+        clock;
+        clock;
+        clock;
+        clock;
+        clock;
+        assert (out === 16'd7 && waiting === 1'b1) begin
             $display("[PASS] ADD r7 r2 r6 lsr#1");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] ADD r7 r2 r6 lsr#1");
             numfail = numfail + 1;
-            error = 1'b1;
+            error   = 1'b1;
         end
 
         //CMP r3 r5 arith rs #1
         instr = 16'b1010100000011111;
         initiate;
-        clock; clock; clock; clock; 
-        assert(out === 16'b1111111111111110 &&{Z, V, N} === 3'b001 && waiting === 1'b1)
-        begin
+        clock;
+        clock;
+        clock;
+        clock;
+        assert ({Z, V, N} === 3'b001 && waiting === 1'b1) begin
             $display("[PASS] CMP r3 r5 arith rs #1");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] CMP r3 r5 arith rs #1");
             numfail = numfail + 1;
-            error = 1'b1;
+            error   = 1'b1;
         end
 
         set_value(16'b1101000010000011);
@@ -194,89 +239,114 @@ module tb_cpu(output err);
         //CMP zero result
         instr = 16'b1010100100000000;
         initiate;
-        clock; clock; clock; clock; 
-        assert(out === 16'b0 && {Z, V, N} === 3'b100 && waiting === 1'b1)
-        begin
+        clock;
+        clock;
+        clock;
+        clock;
+        assert ({Z, V, N} === 3'b100 && waiting === 1'b1) begin
             $display("[PASS] CMP zero result");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] CMP zero result");
             numfail = numfail + 1;
-            error = 1'b1;
-        end 
+            error   = 1'b1;
+        end
 
         set_value(16'b1101000001111111);
         set_value(16'b1101000110000000);
         //CMP neg result
         instr = 16'b1010100100000000;
         initiate;
-        clock; clock; clock; clock; 
-        assert(out === 16'b1111111100000001 && {Z, V, N} === 3'b001 && waiting === 1'b1)
-        begin
+        clock;
+        clock;
+        clock;
+        clock;
+        assert ({Z, V, N} === 3'b001 && waiting === 1'b1) begin
             $display("[PASS] CMP neg result");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] CMP neg result");
             numfail = numfail + 1;
-            error = 1'b1;
+            error   = 1'b1;
         end
 
         // 
         instr = 16'b1100000000111001;
         initiate;
-        clock; clock; clock; clock; 
+        clock;
+        clock;
+        clock;
+        clock;
         instr = 16'b1100000000111001;
         initiate;
-        clock; clock; clock; clock; 
+        clock;
+        clock;
+        clock;
+        clock;
         instr = 16'b1100000000111001;
         initiate;
-        clock; clock; clock; clock; 
+        clock;
+        clock;
+        clock;
+        clock;
         instr = 16'b1100000000111001;
         initiate;
-        clock; clock; clock; clock; 
+        clock;
+        clock;
+        clock;
+        clock;
         instr = 16'b1100000000111001;
         initiate;
-        clock; clock; clock; clock; 
+        clock;
+        clock;
+        clock;
+        clock;
         instr = 16'b1100000000110001;
         initiate;
-        clock; clock; clock; clock; 
+        clock;
+        clock;
+        clock;
+        clock;
         instr = 16'b1011100000100001;
         initiate;
-        clock; clock; clock; clock; 
+        clock;
+        clock;
+        clock;
+        clock;
 
         //CMP underflow
         instr = 16'b1010100100000000;
         initiate;
-        clock; clock; clock; clock; 
-        assert({Z, V, N} === 3'b010 && waiting === 1'b1)
-        begin
+        clock;
+        clock;
+        clock;
+        clock;
+        assert ({Z, V, N} === 3'b010 && waiting === 1'b1) begin
             $display("[PASS] CMP underflow");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] CMP underflow");
             numfail = numfail + 1;
-            error = 1'b1;
+            error   = 1'b1;
         end
 
-        instr = 16'b101010000000001;
+        instr = 16'b1010100000000001;
         initiate;
-        clock; clock; clock; clock; clock; 
-        assert({Z, V, N} === 3'b010 && waiting === 1'b1)
-        begin
+        clock;
+        clock;
+        clock;
+        clock;
+        assert ({Z, V, N} === 3'b010 && waiting === 1'b1) begin
             $display("[PASS] CMP overflow");
             numpass = numpass + 1;
         end
-        else 
-        begin
+        else begin
             $error("[FAIL] CMP overflow");
             numfail = numfail + 1;
-            error = 1'b1;
+            error   = 1'b1;
         end
         #30;
 
@@ -285,4 +355,4 @@ module tb_cpu(output err);
         $display("Number of failed cases: %d", numfail);
         $stop();
     end
-endmodule: tb_cpu
+endmodule : tb_cpu
