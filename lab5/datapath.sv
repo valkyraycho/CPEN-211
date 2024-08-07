df7a20a24e0bb946bff530a8550adae796da5407
Ray Cho <razor0424@gmail.com>
2024-08-07 20:39:52 +0800

    lab5 format

--- lab5/datapath.sv
+++ lab5/datapath.sv
@@ -1,20 +1,44 @@
-module datapath(input clk, input [15:0] datapath_in, input wb_sel,
-                input [2:0] w_addr, input w_en, input [2:0] r_addr, input en_A,
-                input en_B, input [1:0] shift_op, input sel_A, input sel_B,
-                input [1:0] ALU_op, input en_C, input en_status,
-                output [15:0] datapath_out, output Z_out);
+module datapath (
+    input         clk,
+    input  [15:0] datapath_in,
+    input         wb_sel,
+    input  [ 2:0] w_addr,
+    input         w_en,
+    input  [ 2:0] r_addr,
+    input         en_A,
+    input         en_B,
+    input  [ 1:0] shift_op,
+    input         sel_A,
+    input         sel_B,
+    input  [ 1:0] ALU_op,
+    input         en_C,
+    input         en_status,
+    output [15:0] datapath_out,
+    output        Z_out
+);
     wire [15:0] w_data;
     assign w_data = (wb_sel) ? datapath_in : datapath_out;
 
-    wire [15:0] r_data;  
-    regfile registerfile(.w_data, .w_addr, .w_en, .r_addr, .clk, .r_data);
+    wire [15:0] r_data;
+    regfile registerfile (
+        .w_data,
+        .w_addr,
+        .w_en,
+        .r_addr,
+        .clk,
+        .r_data
+    );
 
     reg [15:0] A, B;
     always_ff @(posedge clk) if (en_A) A <= r_data;
     always_ff @(posedge clk) if (en_B) B <= r_data;
-    
+
     wire [15:0] shift_out;
-    shifter shift(.shift_in(B), .shift_op, .shift_out);
+    shifter shift (
+        .shift_in(B),
+        .shift_op,
+        .shift_out
+    );
 
     wire [15:0] val_A, val_B;
     assign val_A = (sel_A) ? 16'b0 : A;
@@ -22,12 +46,18 @@ module datapath(input clk, input [15:0] datapath_in, input wb_sel,
 
     wire [15:0] ALU_out;
     wire Z;
-    ALU alu(.val_A, .val_B, .ALU_op, .ALU_out, .Z);
+    ALU alu (
+        .val_A,
+        .val_B,
+        .ALU_op,
+        .ALU_out,
+        .Z
+    );
 
     reg out_z;
     reg [15:0] out_datapath;
-    assign Z_out = out_z;
+    assign Z_out        = out_z;
     assign datapath_out = out_datapath;
     always_ff @(posedge clk) if (en_C) out_datapath <= ALU_out;
     always_ff @(posedge clk) if (en_status) out_z <= Z;
-endmodule: datapath
+endmodule : datapath
